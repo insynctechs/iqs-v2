34b096871c797f822199ea12beae9345ee3dfab0
insynctechs <linda@insynctechs.com>
2017-10-17 22:15:31 +0530

    Reviews Controllers and Model Start

---
+++ IQSV2/IQSCore/Controllers/ReviewsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using IQSCore.Models;
+
+namespace IQSCore.Controllers
+{
+    public class ReviewsController : ApiController
+    {
+
+
+        Review rev = new Review();
+
+        [Route("api/Reviews/TagReviewHelpful")]
+        public async Task<IHttpActionResult> TagReviewHelpful(int CommentId)
+        {
+            var res = await rev.TagReviewHelpful(CommentId);
+            return Ok(res);
+        }
+
+        [Route("api/Reviews/UpdateReviewRating")]
+        public async Task<IHttpActionResult> UpdateReviewRating(int CommentId, int Rate)
+        {
+            var res = await rev.UpdateReviewRating(CommentId, Rate);
+            return Ok(res);
+        }
+
+
+
+    }
+}
